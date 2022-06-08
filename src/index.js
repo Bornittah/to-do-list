4a0e286cf8d4f0fcb65f8d6a262a0cea71e2ec5b
bornittah <bornittah@gmail.com>
2022-06-08 18:53:04 +0300

    implement add todo

--- src/index.js
+++ src/index.js
@@ -1,38 +1,75 @@
-const todo = [
-  {
-    index: 1,
-    description: 'Take breakfast',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'Attend morning session',
-    completed: true,
-  },
-  {
-    index: 3,
-    description: 'Work on the project',
-    completed: false,
-  },
-  {
-    index: 4,
-    description: 'Take lunch',
-    completed: false,
-  },
-];
-
+const inputField = document.querySelector('#activity'); 
+const form = document.querySelector('.form');
 const list = document.querySelector('.list');
-let str = '';
-todo.map((activity) => {
+
+let todoList = [];
+const add = (todo) => {
+  todoList = [...todoList, todo];
+  console.log(todoList)
+  return todoList.map(function(obj, index) {
+    return Object.assign({}, obj, { index: index+1 });
+  });
+}
+
+const display = (todo) => {
+  let str = '';
   const li = document.createElement('li');
-  li.setAttribute('class', 'list-item');
-  str = `<div>
-            <input type="checkbox" id=${activity.index} >
-            <label for="${activity.index}">${activity.description}</label>
-          </div>
-          <i class="fa-solid fa-ellipsis-vertical"></i>
-          `;
-  li.innerHTML = str;
+  todoList.forEach((activity) => {
+    li.setAttribute('class', 'list-item');
+    str = `<div class="form-group">
+              <input type="checkbox" id=${activity.index} >
+              <label for="${activity.index}">${activity.description}</label>
+            </div>
+           <div class="action-icons">
+              <i class="fa-solid fa-ellipsis-vertical show-more"></i>
+              <i class="fa fa-pen edit"></i>
+              <i class="fa fa-trash-can delete"></i>
+           </div>
+            `;
+    li.innerHTML = str;
+    return li;
+  });
   list.appendChild(li);
-  return list;
+}
+
+const remove = (index) => {
+  todoList.splice(index, 1);
+  console.log(todoList)
+  return todoList;
+}
+
+
+const clearFields = () => {
+  inputField.value = '';
+  return true;
+}
+
+let count = 1;
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const obj = {
+    index: count++,
+    description: inputField.value,
+    completed: false,
+  }
+  add(obj);
+  display(obj);
+  clearFields();
 });
+
+document.querySelectorAll('.list').forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(e.target.classList.contains('delete')){
+    console.log('yes')
+    remove(e.target);
+    }
+    
+  });
+
+});
+
+
+
+
+
