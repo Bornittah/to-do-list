29a32602bd5da9114774265e6cc2972e50d5b9f3
bornittah <bornittah@gmail.com>
2022-06-10 00:54:13 +0300

    add and remove

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 import {Todo} from './modules/storage.js';
-
 const form = document.querySelector('.form');
 let inputField = document.querySelector('#activity'); 
-const completed = false;
-let index = 1;
 
 Todo.display();
-
 form.addEventListener('submit', (e) => {
-  let description = inputField.value;
-  let todo = new Todo(index, description, completed);
-  console.log(description);
   e.preventDefault();
-  Todo.addTodo(todo);
-  Todo.display();
-  description = '';
+  let description = inputField.value;
+  let todo = new Todo(description);
+  todo.addTodo();
 });
 
 document.querySelectorAll('.list').forEach((link, id) => {
   link.addEventListener('click', (e) => {
     e.preventDefault();
-    if(e.target.classList.contains('delete')){
-    console.log(id)
-    Todo.removeTodo(id);
-    }
-    
-  });
 
+    document.querySelectorAll('.delete').forEach((link, id) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        Todo.removeTodo(id);
+      });
+    });
+  });
 });
+
+
 
 
 
--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,65 +1,78 @@
 export class Todo {
-  constructor(index, description, completed) {
-    this.index = index;
+  constructor(description) {
     this.description = description;
-    this.completed = completed;
+  }
+
+  static clearInput = () => {
+    let inputField = document.querySelector('#activity'); 
+    inputField.value = '';
+    return true;
   }
 
   static getTodo = () => {
-    let todoList;
+    let todoList = [];
     let data = localStorage.getItem('todo') 
     if (data === null) {
-      todoList = [];
+      localStorage.setItem('todo', JSON.stringify(todoList));
     }
     todoList = JSON.parse(localStorage.getItem('todo'));
     return todoList;
   }
 
-  static addTodo = (todo) => {
-    let todoList = Todo.getTodo();
-    todoList = [...todoList, todo];
-    localStorage.setItem('todo', JSON.stringify(todoList));
+  static getIndex = () => {
+    const todoList = Todo.getTodo();
+   let index=0;
+    if(todoList === null){
+      return index+1;
+    }
+    return index = todoList.length+1;
+  }
+
+    addTodo = () => {
+    let data = Todo.getTodo();
+    const index= Todo.getIndex();
+
+    const todo = {
+      index: index,
+      description: this.description,
+      completed: false,
+    };
+  
+    if (data === null) {
+      data.push(todo);
+      localStorage.setItem('todo', JSON.stringify(todoList))
+    }
+    let newtodoList = JSON.parse(localStorage.getItem('todo'));
+    newtodoList = [...data, todo];
+    localStorage.setItem('todo', JSON.stringify(newtodoList));
+    Todo.clearInput();
+    Todo.display();
   }
 
   static display() {
-    const todoList = Todo.getTodo();
-    console.log(todoList)
-    let str = '';
-   
+    const todoList =  Todo.getTodo();
+    const list = document.querySelector('.list');
+    let str='';
     todoList.forEach((todo) => {
-      const list = document.querySelector('.list');
-      const li = document.createElement('li');
-      li.setAttribute('class', 'list-item');
-      str = `<div class="form-group">
-                <input type="checkbox" id=${todo.index}>
-                <label for="${todo.index}">${todo.description}</label>
-              </div>
-             <div class="action-icons">
-                <i class="fa-solid fa-ellipsis-vertical show-more"></i>
-                <i class="fa fa-pen edit"></i>
-                <i class="fa fa-trash-can delete"></i>
-             </div>
-              `;
-      li.innerHTML = str;
-      list.appendChild(li);
-      console.log(li)
+    str += `<li class="list-item">
+            <div class="form-group">
+            <input type="checkbox" id="${todo.index}">
+            <label for="${todo.index}">${todo.description}</label>
+          </div>
+          <div class="action-icons">
+              <i class="fa-solid fa-ellipsis-vertical show-more"></i>
+              <i class="fa fa-pen edit"></i>
+              <i class="fa fa-trash-can delete"></i>
+          </div>
+          </li>`;
     });
-   
-    
-    
+    list.innerHTML = str;
   }
 
   static removeTodo = (index) => {
-    // const todo = Todo.getTodo(index);
-    // todoList.splice(index, 1);
-    // console.log(todoList)
-    // localStorage.setItem('todo', JSON.stringify(todoList));
-    let data = JSON.parse(localStorage.getItem('todo'));
-    const selectedTodo = data[index];
-    const filteredList = data.filter((item) => item !== selectedTodo);
-    localStorage.setItem('todo', JSON.stringify(filteredList));
-    const newData = JSON.parse(localStorage.getItem('todo'));
-    data = newData;
+    const todoList = Todo.getTodo(index);
+    todoList.splice(index, 1);
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    Todo.display();
   }
-
 }
