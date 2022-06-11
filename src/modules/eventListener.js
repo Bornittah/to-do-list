5ffb80d88c6bebc34cd22889e9939280afbfb643
bornittah <bornittah@gmail.com>
2022-06-11 21:55:48 +0300

    fix linter errors

--- src/modules/eventListener.js
+++ src/modules/eventListener.js
@@ -2,21 +2,21 @@ import { Todo } from './storage.js';
 
 export default function addEventListenersToListItems() {
   document.querySelectorAll('.checkbox').forEach((link) => {
-      link.addEventListener('click', (e) => {
-          link.nextElementSibling.classList.toggle('line-through');
-          Todo.completed(link.id, e.target.checked);
-      });
+    link.addEventListener('click', (e) => {
+        link.nextElementSibling.classList.toggle('line-through');
+        Todo.completed(link.id, e.target.checked);
+    });
   });
   document.querySelectorAll('.textarea').forEach((link, index) => {
-      link.addEventListener('keyup', (e) => {
-          e.preventDefault();
-          Todo.updateTodo(index, e.target.value);
-      });
+    link.addEventListener('keyup', (e) => {
+        e.preventDefault();
+        Todo.updateTodo(index, e.target.value);
+    });
   });
   document.querySelectorAll('.delete').forEach((link, index) => {
-      link.addEventListener('click', (e) => {
-          e.preventDefault();
-          Todo.removeTodo(index);
-      });
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        Todo.removeTodo(index);
+    });
   });
 }
