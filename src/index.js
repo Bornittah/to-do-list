b06868b9a117c3e68aea06dd2cd1e2733bf7e60e
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-06-11 18:00:53 +0100

    Add edit functions

--- src/index.js
+++ src/index.js
@@ -1,67 +1,90 @@
-import { Todo } from './modules/storage.js';
+
+import { editTodo, removeTodo } from './storage';
+import { getFromLocal, updateLocalStorage } from './TodoStatus';// eslint-disable-line
 import './style.css';
-import './modules/fontawesome/css/all.css';
 
-const form = document.querySelector('.form');
-const inputField = document.querySelector('#activity');
-const submitBtn = document.querySelector('#submit');
-const checkbox = document.querySelectorAll('.checkbox');
+// Queries to HTML
+const inputText = document.querySelector('input');
+const todoMainContainer = document.querySelector('.todos-container');
+const ClearBtn = document.querySelector('button');
 
-Todo.display();
-Todo.checkedTask();
+// class object
+class MyObjects {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+}
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const description = inputField.value;
-  const todo = new Todo(description);
-  todo.addTodo();
+// Array of Objects
+export const myArray = [];
+
+// Add Methods
+const addTodo = (todoValue) => {
+  const todoContainer = document.createElement('div');
+  todoContainer.className = 'todoContainer';
+  todoContainer.innerHTML += `
+    <input type='checkbox' class='checkbox'>
+    <span>${todoValue}</span>
+    <i class='fas fa-ellipsis-v'></i>
+    <i class='fas fa-trash-alt'></i>
+  `;
+  todoMainContainer.appendChild(todoContainer);
+  const checkbox = document.querySelectorAll('.checkbox');
+  checkbox.forEach((i) => {
+    i.addEventListener('click', () => {
+      i.parentElement.classList.toggle('checkedContainer');
+      i.nextElementSibling.classList.toggle('checkToDo');
+      i.parentElement.lastElementChild.classList.toggle('trash-active');
+      i.parentElement.lastElementChild.previousElementSibling.classList.toggle('edited-disable');
+      updateLocalStorage();
+    });
+  });
+
+  // Objects
+  const object = new MyObjects(todoValue, false, checkbox.length - 1);
+  myArray.push(object);
+  localStorage.setItem('list', JSON.stringify(myArray));
+
+  const EditIcons = document.querySelectorAll('.fa-ellipsis-v');
+  EditIcons.forEach((i) => {
+    i.addEventListener('click', () => {
+      editTodo(todoContainer, i.previousElementSibling);
+      i.parentElement.classList.add('checkedContainer');
+    });
+  });
+
+  const removeIcons = document.querySelectorAll('.fa-trash-alt');
+  removeIcons.forEach((i) => {
+    i.addEventListener('click', () => {
+      removeTodo(i.parentElement);
+    });
+  });
+};
+
+inputText.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && inputText.value) {
+    // e.preventDefault();
+    addTodo(inputText.value);
+    inputText.value = null;
+  }
 });
 
-inputField.addEventListener('focus', (e) => {
-  e.preventDefault();
-  submitBtn.style.display = 'block';
-});
+getFromLocal();
 
-document.querySelector('#submit').addEventListener('click', (e) => {
-  e.preventDefault();
-  const description = inputField.value;
-  const todo = new Todo(description);
-  todo.addTodo();
-});
-
-document.querySelectorAll('.checkbox').forEach((link, id) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (e.target.checked === true) {
-      Todo.completed(id, true);
-    } else {
-    Todo.completed(id, false);
+// Clear all function
+const ClearAllBtn = () => {
+  const localData = JSON.parse(localStorage.getItem('list'));
+  const todoContainer = document.querySelectorAll('.todoContainer');
+  todoContainer.forEach((i) => {
+    if (i.classList.contains('checkedContainer')) {
+      removeTodo(i);
     }
   });
-});
-
-document.querySelectorAll('#textarea').forEach((link, id) => {
-  link.addEventListener('keyup', (e) => {
-    e.preventDefault();
-    Todo.updateTodo(id, e.target.value);
-  });
-});
-
-document.querySelectorAll('.delete').forEach((link, id) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    Todo.removeTodo(id);
-  });
-});
-
-document.querySelectorAll('.clear').forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    Todo.clearCompleted();
-  });
-});
-
-document.querySelector('.reset').addEventListener('click', (e) => {
-  e.preventDefault();
-  Todo.reset();
-});
+  let count = 0;
+  const data = Array.from(localData).filter(i => i.completed === false);// eslint-disable-line
+  data.map(i.index = count += 1); // eslint-disable-line
+  localStorage.setItem('list', JSON.stringify(data));
+};
+ClearBtn.addEventListener('click', ClearAllBtn);
